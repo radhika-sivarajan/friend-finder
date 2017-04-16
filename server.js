929c06fcafd2d34ecea4d4da756096ebe74c98f2
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-16 01:12:34 -0700

    Indentation.

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.use(express.static('app'));
 
-require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
