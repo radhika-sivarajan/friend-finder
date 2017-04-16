990db8d5524801dcc1cb1ff1decdd6db6840ad2f
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-16 01:07:56 -0700

    File path corrected.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var app = express();
-var PORT = process.env.PORT || 8080;
+
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+app.use(express.static('app'));
+
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
