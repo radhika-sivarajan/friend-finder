7d9a60c041f3ed368b327a1c322f0145df8b8e2a
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-15 22:53:46 -0700

    friends list

---
+++ app/data/friends.js
@@ -0,0 +1,76 @@
+var friends = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        4,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        4
+    ]
+}, {
+    "name": "Annie",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        2,
+        1,
+        4,
+        3,
+        5,
+        1,
+        2,
+        5,
+        1,
+        1
+    ]
+}, {
+    "name": "Alex",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        2,
+        4,
+        3,
+        5,
+        1,
+        4,
+        6,
+        1,
+        1
+    ]
+}, {
+    "name": "Beena",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        1,
+        2,
+        3,
+        1,
+        1,
+        2,
+        4,
+        4,
+        2
+    ]
+}, {
+    "name": "Rahul",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        1,
+        3,
+        1,
+        1,
+        1,
+        3,
+        4,
+        2,
+        2,
+        3
+    ]
+}];
