ac8344f0cdb4a8cf3e51b69dfbf88c7e8e9627a1
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-17 11:20:39 -0700

    More frieds added.

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,22 @@ var friends = [{
         1
     ]
 }, {
-    "name": "Sam",
+    "name": "Deepika Padukone",
+    "photo": "https://s-media-cache-ak0.pinimg.com/736x/73/74/49/737449e8b3225c09289f865b2e349a64.jpg",
+    "scores": [
+        2,
+        5,
+        1,
+        3,
+        2,
+        1,
+        1,
+        5,
+        2,
+        3
+    ]
+}, {
+    "name": "Zac Efron",
     "photo": "http://dailynewsdig.com/wp-content/uploads/2014/10/10-Hollywood-Actors-Who-Hate-Watching-Their-Own-Movies-6.jpg",
     "scores": [
         4,
@@ -29,7 +44,7 @@ var friends = [{
         4
     ]
 }, {
-    "name": "Alex",
+    "name": "Alex Mathew",
     "photo": "http://inminutes.com/wp-content/uploads/2015/01/Top-Ten-Most-Handsome-Actors-In-Hollywood-2014-9.jpg",
     "scores": [
         5,
@@ -44,7 +59,7 @@ var friends = [{
         1
     ]
 }, {
-    "name": "Jennifer",
+    "name": "Jennifer Wiget",
     "photo": "http://starsunfolded.1ygkv60km.netdna-cdn.com/wp-content/uploads/2016/04/Jennifer-Winget.jpg",
     "scores": [
         5,
@@ -59,7 +74,127 @@ var friends = [{
         2
     ]
 }, {
-    "name": "Tom",
+    "name": "Aishwarya Rai",
+    "photo": "http://www.pinkvilla.com/files/styles/contentpreview/public/khygyf.JPG",
+    "scores": [
+        2,
+        2,
+        2,
+        1,
+        5,
+        1,
+        3,
+        5,
+        1,
+        2
+    ]
+}, {
+    "name": "Douglas Booth",
+    "photo": "http://www.worldstopmost.com/wp-content/uploads/2016/06/Douglas-Booth-Most-Handsome-Hollywood-Actors-2017.jpg",
+    "scores": [
+        2,
+        3,
+        3,
+        1,
+        1,
+        1,
+        2,
+        3,
+        1,
+        4
+    ]
+}, {
+    "name": "Will Smith",
+    "photo": "http://www.strongestinworld.com/wp-content/uploads/2016/09/Will-Smith.jpg",
+    "scores": [
+        4,
+        2,
+        2,
+        2,
+        5,
+        3,
+        3,
+        3,
+        5,
+        5
+    ]
+}, {
+    "name": "Mustafa Burmawalla",
+    "photo": "http://youngsterchoice.com/wp-content/uploads/2017/03/Mustafa-Burmawalla-Wiki.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        4,
+        1,
+        1,
+        3,
+        5,
+        2,
+        2
+    ]
+}, {
+    "name": "Rachel Mcdams",
+    "photo": "http://vignette4.wikia.nocookie.net/rachelmcadams/images/d/da/Tumblr_mdy6vaRwrC1qa9nc2o1_500.jpg",
+    "scores": [
+        1,
+        1,
+        4,
+        3,
+        5,
+        1,
+        2,
+        1,
+        1,
+        1
+    ]
+}, {
+    "name": "Emma Watson",
+    "photo": "https://s-media-cache-ak0.pinimg.com/736x/a8/18/7b/a8187bebb2b81213dc1718a0e9d9cf9d.jpg",
+    "scores": [
+        3,
+        1,
+        3,
+        4,
+        1,
+        1,
+        4,
+        2,
+        4,
+        3
+    ]
+}, {
+    "name": "Hrithik Roshan",
+    "photo": "http://wallpapersdsc.net/wp-content/uploads/2015/12/1499.jpg",
+    "scores": [
+        5,
+        3,
+        2,
+        4,
+        1,
+        1,
+        2,
+        3,
+        1,
+        1
+    ]
+}, {
+    "name": "Cillian Murphy",
+    "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2015-05/8/19/enhanced/webdr15/enhanced-2900-1431127898-1.jpg",
+    "scores": [
+        3,
+        1,
+        2,
+        4,
+        5,
+        1,
+        3,
+        1,
+        4,
+        1
+    ]
+}, {
+    "name": "Tom Cruise",
     "photo": "http://top10for.com/wp-content/uploads/2015/08/Tom-Cruise-a-richest-actor-in-Hollywood.jpg?2698f2",
     "scores": [
         1,
