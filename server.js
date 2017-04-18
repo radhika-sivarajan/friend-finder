794322923038c078399aa296132d884e2e36c6d9
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-18 13:59:56 -0700

    Comments and staic folder path changed.

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Creating an express server
 var app = express();
 
+// Set an initial port
 var PORT = process.env.PORT || 3000;
 
+// BodyParser makes it possible for our server to interpret data sent to it.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-app.use(express.static('app'));
+// Serving static files in Express (path of static file sfolder)
+app.use(express.static('app/public'));
 
+// The below points our server to a series of "route" files.
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
